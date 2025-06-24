20050beb74fa19d69cf1b60a2bd92b728ddbb5b2
Joyce Cruz <joycedacruzoliveira@gmail.com>
2025-06-24 21:15:52 +0000

    primeiro commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById("botaoEnviar").addEventListener("click", validarFormulario)
+
+function validarFormulario() {
+
+  var nome = document.getElementById("nome").value;
+  var email = document.getElementById("email").value;
+  var telefone = document.getElementById("telefone").value;
+
+  
+    if (nome != "" && email != "" && telefone != "") {
+      alert("Prontinho! Você receberá as novidades por email.")
+    } else {
+      alert("Por favor, preencha os campos nome, email e telefone!")
+    }
+}
+
+
